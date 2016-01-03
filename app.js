025b6212510b16b6ef15e099db6e4cde09de9253
Niyas Mohammed A <niaz.angels@gmail.com>
2016-01-03 15:33:36 +0000

    Bug on serial port open fail corrected

--- app.js
+++ app.js
@@ -36,34 +36,42 @@ status_b = false;
 status_c = false;
 status_d = false;
 
-io.sockets.on('connection', function (socket) {
-	socket.on('move', function (data) {
-		console.log ("Moving " + data.value)
-		serialPort.write(data.value)
-	});
-
-	socket.on('action', function (data) {
-
-		// Change the states of devices
-		switch (data.button){
-			case 'a' : status_a = !status_a; break;
-			case 'b' : status_b = !status_b; break;
-			case 'c' : status_c = !status_c; break;
-			case 'd' : status_d = !status_d; break;
-			default: break;
+serialPort.open(function (error) {
+  if (error) {
+   		console.log('Failed to open SerialPort: ' + error);
+  } else {
+		console.log('Serial port opened');
+	    // Always listen actively for data. 
+		// If heart_rate, broadcast its value.
+		
+		var bpm_value = ''; // this stores the filtered data
+		var readData = '';  // this stores the raw data
+		
+		 // Call back when data is received
+		serialPort.on('data', function (data) {
+		
+		// Append data to buffer
+		readData += data.toString(); 
+		
+		// If the Letters '*' and '#' are found on the buffer 
+		// then isolate what's in the middle, which is the BPM
+		// Then clear the read buffer.
+		if (readData.indexOf('*') >= 0 && readData.indexOf('#') >= 0) {
+			bpm_value = readData.substring(readData.indexOf('*') + 1, readData.indexOf('#'));
+			readData = '';
+			bpm_value = parseInt(bpm_value,10);
+			if(bpm_value >= 60 && bpm_value <= 100)
+			{
+				weighted_bpm = bpm_value;
+			}
+			weighted_bpm = parseInt(weighted_bpm,10);
+			console.log('BPM: ' + weighted_bpm );
+			// To introduce filtering of bpm
+			// io.sockets.emit('heart_rate', {'value': weighted_bpm});
+			io.sockets.emit('heart_rate', {'value': bpm_value});
 		}
-		console.log ("\nAction set is now")
-		console.log (status_a, status_b, status_c, status_d);
-
-		// Send the toggled button blueooth module
-		bluetooth.write(data.button)
-
-		// Now emit to all connected devices so they synchronise
-		io.sockets.emit('action', { 'a': status_a, 'b': status_b, 'c': status_c, 'd': status_d });
-	});
-
-	// Emit status when new devices are connected
-	socket.emit('action', { 'a': status_a, 'b': status_b, 'c': status_c, 'd': status_d });
+    });
+  }
 });
 
 bluetooth.on('open', function (error) {
@@ -105,43 +113,33 @@ bluetooth.on('open', function (error) {
 	}
 });
 
-serialPort.open(function (error) {
-  if (error) {
-   		console.log('Failed to open SerialPort: ' + error);
-  } else {
-		
-		// Reset's the states of all devices
-		bluetooth.write('x');
+io.sockets.on('connection', function (socket) {
+	socket.on('move', function (data) {
+		console.log ("Moving " + data.value)
+		serialPort.write(data.value)
+	});
 
-	    // Always listen actively for data. 
-		// If heart_rate, broadcast its value.
-		
-		var bpm_value = ''; // this stores the filtered data
-		var readData = '';  // this stores the raw data
-		
-		 // Call back when data is received
-		serialPort.on('data', function (data) {
-		
-		// Append data to buffer
-		readData += data.toString(); 
-		
-		// If the Letters '*' and '#' are found on the buffer 
-		// then isolate what's in the middle, which is the BPM
-		// Then clear the read buffer.
-		if (readData.indexOf('*') >= 0 && readData.indexOf('#') >= 0) {
-			bpm_value = readData.substring(readData.indexOf('*') + 1, readData.indexOf('#'));
-			readData = '';
-			bpm_value = parseInt(bpm_value,10);
-			if(bpm_value >= 60 && bpm_value <= 100)
-			{
-				weighted_bpm = bpm_value;
-			}
-			weighted_bpm = parseInt(weighted_bpm,10);
-			console.log('BPM: ' + weighted_bpm );
-			// To introduce filtering of bpm
-			// io.sockets.emit('heart_rate', {'value': weighted_bpm});
-			io.sockets.emit('heart_rate', {'value': bpm_value});
+	socket.on('action', function (data) {
+
+		// Change the states of devices
+		switch (data.button){
+			case 'a' : status_a = !status_a; break;
+			case 'b' : status_b = !status_b; break;
+			case 'c' : status_c = !status_c; break;
+			case 'd' : status_d = !status_d; break;
+			default: break;
 		}
-    });
-  }
+		console.log ("\nAction set is now")
+		console.log (status_a, status_b, status_c, status_d);
+
+		// Send the toggled button blueooth module
+		bluetooth.write(data.button)
+
+		// Now emit to all connected devices so they synchronise
+		io.sockets.emit('action', { 'a': status_a, 'b': status_b, 'c': status_c, 'd': status_d });
+	});
+
+	// Emit status when new devices are connected
+	socket.emit('action', { 'a': status_a, 'b': status_b, 'c': status_c, 'd': status_d });
 });
+
